a9d0f58f5147c6cdfedc13a4a257b8d3b667b960
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-19 18:26:11 -0500

    Codigo inicial con application properties establecidos

---
+++ src/main/java/com/example/nb/SpringAutenticacionBasicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutenticacionBasicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutenticacionBasicaApplication.class, args);
+    }
+
+}
