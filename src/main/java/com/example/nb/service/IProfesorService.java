f8a7871905ca861e8a20862cb04ef994eb17836e
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-19 18:31:58 -0500

    Service

---
+++ src/main/java/com/example/nb/service/IProfesorService.java
@@ -0,0 +1,11 @@
+package com.example.nb.service;
+
+import com.example.nb.entity.Profesor;
+
+import java.util.List;
+
+public interface IProfesorService {
+
+    List<Profesor> findAllProfesor();
+    
+}
