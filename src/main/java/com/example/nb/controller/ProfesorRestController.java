163c8828354a8717c955b088d7966f63542deda5
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-19 18:40:29 -0500

    Implementando controller

---
+++ src/main/java/com/example/nb/controller/ProfesorRestController.java
@@ -0,0 +1,28 @@
+package com.example.nb.controller;
+
+import com.example.nb.entity.Profesor;
+import com.example.nb.service.ProfesorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProfesorRestController {
+
+    @Autowired
+    private ProfesorServiceImpl profesorService;
+
+    @RequestMapping(value = "/todos_los_profesores", method = RequestMethod.GET)
+    public ResponseEntity<List<Profesor>> listAllProfesor() {
+        List<Profesor> profesores = profesorService.findAllProfesor();
+        if (profesores.isEmpty()) {
+            return new ResponseEntity<List<Profesor>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Profesor>>(profesores, HttpStatus.OK);
+    }
+}
