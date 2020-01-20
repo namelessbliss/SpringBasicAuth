c3b59c9a0a4d67a163e877ff0399f418fec8e864
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-19 21:10:31 -0500

    Complementando Config Security

--- src/main/java/com/example/nb/security/SecurityConfig.java
+++ src/main/java/com/example/nb/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.nb.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.naming.AuthenticationException;
 
@@ -27,5 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/*/escribirDB/**").hasRole("ADMIN")
                 .and()
                 .httpBasic();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
